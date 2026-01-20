68dc5f5a855fe6bd8ed56023f36f2a769382b0a8
Amy Opas <amy.opas@proton.me>
2026-01-20 21:50:21 +0100

    Added a csv export for frametime data

--- examples/particle-benchmark/src/main.rs
+++ examples/particle-benchmark/src/main.rs
@@ -1,24 +1,28 @@
-use std::io;
+use std::io::Write;
+use std::{fs::File, io};
 
 use germterm::{
     color::Color,
     crossterm::event::{Event, KeyCode, KeyEvent, KeyEventKind},
-    draw::{draw_text, fill_screen},
+    draw::fill_screen,
     engine::{Engine, end_frame, exit_cleanup, init, start_frame},
     fps_counter::draw_fps_counter,
     input::poll_input,
-    particle::{ParticleEmitter, ParticleSpec, spawn_particles},
+    particle::{ParticleColor, ParticleEmitter, ParticleSpec, spawn_particles},
 };
 
 pub const TERM_COLS: u16 = 40;
 pub const TERM_ROWS: u16 = 20;
+pub const PARTICLE_COUNT: usize = 100_000;
 
 fn main() -> io::Result<()> {
     let mut engine: Engine = Engine::new(TERM_COLS, TERM_ROWS)
         .title("particle-benchmark")
-        .limit_fps(0);
+        .limit_fps(240);
 
     init(&mut engine)?;
+
+    let mut frametimes: Vec<f32> = Vec::with_capacity(200_000);
 
     'update_loop: loop {
         start_frame(&mut engine);
@@ -40,13 +44,13 @@ fn main() -> io::Result<()> {
                         TERM_COLS as f32 * 0.5,
                         TERM_ROWS as f32 * 0.5,
                         &ParticleSpec {
-                            lifetime_sec: 10.0,
-                            speed: 0.5..=50.0,
-                            gravity_scale: 0.02,
-                            ..Default::default()
+                            color: ParticleColor::Solid(Color::VIOLET),
+                            lifetime_sec: 4.0,
+                            speed: 0.5..=35.0,
+                            gravity_scale: 0.01,
                         },
                         &ParticleEmitter {
-                            count: 100_000,
+                            count: PARTICLE_COUNT,
                             ..Default::default()
                         },
                     );
@@ -55,9 +59,16 @@ fn main() -> io::Result<()> {
             }
         }
 
-        draw_fps_counter(&mut engine, 0, 0);
+        frametimes.push(engine.delta_time);
+        draw_fps_counter(&mut engine, 0, 1);
 
         end_frame(&mut engine)?;
+    }
+
+    let mut file = File::create("examples/particle-benchmark/frametimes.csv")?;
+    writeln!(file, "frametime_ms")?;
+    for ft in frametimes {
+        writeln!(file, "{:.4}", ft * 1000.0)?;
     }
 
     exit_cleanup(&mut engine)?;
