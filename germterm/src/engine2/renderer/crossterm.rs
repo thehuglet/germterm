92b6714310ed49aabc1ad7924ba3fe5a37ea5e2b
Tayfun Bocek <tayfunbocek@live.ca>
2026-02-18 21:34:24 +0300

    finish crossterm renderer

--- germterm/src/engine2/renderer/crossterm.rs
+++ germterm/src/engine2/renderer/crossterm.rs
@@ -2,12 +2,32 @@ use std::io::{self, Write};
 
 use crossterm::{cursor, event, execute, terminal};
 
+use crate::engine2::renderer::Renderer;
+
 struct CrosstermRenderer<W: Write> {
     out: W,
 }
 
-impl<W: Write> CrosstermRenderer<W> {
-    pub fn exit(&mut self) -> io::Result<()> {
+// TODO: make the init/restore customizable
+impl<W: Write> Renderer for CrosstermRenderer<W> {
+    type Error = std::io::Error;
+
+    fn start_frame(&mut self) -> Result<(), Self::Error> {
+        terminal::enable_raw_mode()?;
+        execute!(
+            &mut self.out,
+            terminal::EnterAlternateScreen,
+            terminal::DisableLineWrap,
+            cursor::Hide,
+            event::EnableMouseCapture,
+        )
+    }
+
+    fn init(&mut self) -> Result<(), Self::Error> {
+        Ok(())
+    }
+
+    fn restore(&mut self) -> Result<(), Self::Error> {
         terminal::disable_raw_mode()?;
         execute!(
             &mut self.out,
@@ -15,7 +35,16 @@ impl<W: Write> CrosstermRenderer<W> {
             terminal::EnableLineWrap,
             cursor::Show,
             event::DisableMouseCapture,
-        )?;
-        Ok(())
+        )
+    }
+
+    fn end_frame(&mut self) -> Result<(), Self::Error> {
+        self.out.flush()
+    }
+    fn render<'a>(
+        &mut self,
+        calls: impl Iterator<Item = crate::engine2::DrawCall<'a>>,
+    ) -> Result<(), Self::Error> {
+        todo!()
     }
 }
