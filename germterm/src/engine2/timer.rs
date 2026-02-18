4a8f2710455319029906fbaffb77a7cd5f2fbf15
Tayfun Bocek <tayfunbocek@live.ca>
2026-02-18 20:58:40 +0300

    add total for delta types

--- germterm/src/engine2/timer.rs
+++ germterm/src/engine2/timer.rs
@@ -43,7 +43,6 @@ impl NoDelta {
         Self(PhantomData)
     }
 }
-impl TimerDelta for NoDelta {}
 
 pub struct Timer<Timer: FrameTimer> {
     pub(crate) timer: Timer,
@@ -53,6 +52,16 @@ pub struct Timer<Timer: FrameTimer> {
 
 pub type Delta = f32;
 // TODO: maybe seal this trait??
-pub trait TimerDelta: Copy {}
-impl TimerDelta for Delta {}
-impl TimerDelta for NoTimer {}
+pub trait TimerDelta: Copy {
+    fn total(lhs: Self, rhs: Self) -> Self;
+}
+impl TimerDelta for Delta {
+    fn total(lhs: Self, rhs: Self) -> Self {
+        lhs + rhs
+    }
+}
+impl TimerDelta for NoDelta {
+    fn total(_lhs: Self, _rhs: Self) -> Self {
+        Self::new()
+    }
+}
